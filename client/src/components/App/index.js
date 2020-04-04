2e78914de9e25aaf3d90710cbfe6e2e558bbab01
yossefaz <yossefaz@protonmail.com>
2020-04-04 22:22:17 +0300

    create folder tree

---
+++ client/src/components/App/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import {BrowserRouter, Route, Link} from 'react-router-dom'
+
+
+const App = () => {
+    return ( 
+    
+    <div>App</div> 
+    
+    );
+}
+ 
+export default App;
