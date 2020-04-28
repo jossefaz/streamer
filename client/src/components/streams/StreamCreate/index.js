7ceac7b02e3e5ae1d68d8a264a3a55bd1ac7af0d
yossefazoulay <azoulayos@protonmail.com>
2020-04-28 19:41:34 +0300

    adding custom props to redux form field

--- client/src/components/streams/StreamCreate/index.js
+++ client/src/components/streams/StreamCreate/index.js
@@ -1,16 +1,22 @@
 import React, { Component } from 'react'
 import { Field, reduxForm } from 'redux-form'
 class StramCreate extends Component {
-    renderInput({ input }) {
-        return <input {...input} />;
+    renderInput({ input, label }) {
+        return (
+            <div className="field">
+                <label>{label}</label>
+                <input {...input} />
+            </div>
+
+        );
     }
 
     render() {
         console.log(this.props)
         return (
-            <form>
-                <Field name="Title" component={this.renderInput} />
-                <Field name="description" component={this.renderInput} />
+            <form className="ui form">
+                <Field name="Title" component={this.renderInput} label="Enter Title" />
+                <Field name="description" component={this.renderInput} label="Enter description" />
             </form>
         )
     }
