936ca8fbd369b48d81af62842dc77f8e986b4494
yossefazoulay <azoulayos@protonmail.com>
2020-04-28 12:14:18 +0300

    added listerner to gapi

--- client/src/components/Auth/GoogleAuth.js
+++ client/src/components/Auth/GoogleAuth.js
@@ -1,7 +1,9 @@
 import React from 'react';
 
 class GoogleAuth extends React.Component {
-    state = {}
+    state = {
+        isSignedIn: null
+    }
 
     componentDidMount() {
         window.gapi.load('client:auth2', () => {
@@ -9,14 +11,32 @@ class GoogleAuth extends React.Component {
             window.gapi.client.init({
                 clientId: "150782612991-pemfub5je86dsds9j2uqmi4g7jrff82f.apps.googleusercontent.com",
                 scope: 'email'
+            }).then(() => {
+                this.auth = window.gapi.auth2.getAuthInstance();
+                this.setState({ isSignedIn: this.auth.isSignedIn.get() })
+                this.auth.isSignedIn.listen(this.onAuthChange)
             })
         })
     }
 
+    onAuthChange = () => {
+        this.setState({ isSignedIn: this.auth.isSignedIn.get() })
+    }
+
+    renderAuthButton() {
+
+        switch (this.state.isSignedIn) {
+            case null:
+                return <div>No Idea</div>
+            case true:
+                return <div>Signed In</div>
+            default:
+                return <div>Not Signed In</div>;
+        }
+    }
+
     render() {
-        return (
-            <div>Google AUth</div>
-        );
+        return (<div>{this.renderAuthButton()}</div>);
     }
 }
 
