94fbe46b78f8739d3a05ab6feb76adf5a6d00694
yossefazoulay <azoulayos@protonmail.com>
2020-04-28 19:02:54 +0300

    install and init redux-form package

--- client/src/redux/reducers/index.js
+++ client/src/redux/reducers/index.js
@@ -1,3 +1,4 @@
 import { combineReducers } from "redux";
+import { reducer as reduxFormReducer } from 'redux-form'
 import auth from './auth'
-export default combineReducers({ auth })
+export default combineReducers({ auth, form: reduxFormReducer })
