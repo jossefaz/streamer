6161708db17f6a28aee378426b4e1244a4a5a27a
yossefazoulay <azoulayos@protonmail.com>
2020-04-28 18:31:17 +0300

    add redux dev tools

--- client/src/index.js
+++ client/src/index.js
@@ -3,10 +3,13 @@ import ReactDOM from 'react-dom';
 
 import App from './components/App'
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import reducers from './redux/reducers';
 
-const store = createStore(reducers);
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const store = createStore(reducers, composeEnhancers(applyMiddleware()));
 
 
 
