044a0662dd94bfbaafaed8bfab3e335d6dbb6055
yossefazoulay <azoulayos@protonmail.com>
2020-04-29 17:34:01 +0300

    handling form validation with validate function overrided from reduxform

--- client/src/components/streams/StreamCreate/index.js
+++ client/src/components/streams/StreamCreate/index.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react'
 import { Field, reduxForm } from 'redux-form'
+
+
 class StramCreate extends Component {
-    renderInput({ input, label }) {
+    renderInput({ input, label, meta }) {
         return (
             <div className="field">
                 <label>{label}</label>
                 <input {...input} />
+                <div>{meta.errors}</div>
             </div>
 
         );
@@ -15,10 +18,12 @@ class StramCreate extends Component {
         console.log(formValues)
     }
 
+
+
     render() {
         return (
             <form onSubmit={this.props.handleSubmit(this.onSubmit)} className="ui form">
-                <Field name="Title" component={this.renderInput} label="Enter Title" />
+                <Field name="title" component={this.renderInput} label="Enter Title" />
                 <Field name="description" component={this.renderInput} label="Enter description" />
                 <button className="ui button primary">Submit</button>
             </form>
@@ -27,6 +32,18 @@ class StramCreate extends Component {
     }
 }
 
+const validate = (formValues) => {
+    const errors = {};
+    if (!formValues.title) {
+        errors.title = 'A title is mandatory';
+    }
+    if (!formValues.description) {
+        errors.description = 'A description is mandatory';
+    }
+    return errors;
+}
+
 export default reduxForm({
-    form: 'streamCreate'
+    form: 'streamCreate',
+    validate
 })(StramCreate);
